ad725dd1cdb9ac0c1cb9eab34c2b67dbdccbd994
Rakesh Yadav <rryadav142001@gmail.com>
2024-11-14 14:24:18 +0530

    initial setup done

---
+++ OnboardingInSwiftUI/OnboardingInSwiftUI/OnboardingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingInSwiftUIApp.swift
+//  OnboardingInSwiftUI
+//
+//  Created by Rakesh Yadav on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
